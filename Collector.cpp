a6eb8da6194862c5c4b1a172e7980b1b00d11c27
ChrisRC7 <chrisrc7@live.com>
2023-03-11 23:43:07 -0600

    Doxygen comments

--- Collector.cpp
+++ Collector.cpp
@@ -1,34 +1,59 @@
 #include <bits/stdc++.h>
 #include <iostream>
-#include "Node.cpp"
+#include "Node.cpp" 
+/* It creates a linked list that stores integers */
 class Collector
 {
+/* Creating a private variable called head that is a pointer to a Node. */
 private:
     Node *head;
 
+/* Creating a private variable called size that is an integer. */
 private:
     int size;
 
+/* Creating a constructor for the class. */
 public:
+    /**
+     * This function is a constructor for the Collector class
+     */
     Collector()
     {
         this->head = NULL;
         this->size = 0;
     };
 
+/* A function that returns true if the size of the list is 0, otherwise it returns false. */
 public:
+    /**
+     * It returns true if the size of the list is 0, otherwise it returns false.
+     * 
+     * @return A boolean value.
+     */
     bool isEmpty()
     {
         return this->size == 0;
     };
 
+/* Returning the size of the array. */
 public:
+    /**
+     * It returns the size of the array.
+     * 
+     * @return The size of the array.
+     */
     int Size()
     {
         return this->size;
     };
 
+/* Creating a new node and setting the next node to the head. */
 public:
+    /**
+     * It inserts a new node at the beginning of the list.
+     * 
+     * @param data The data that you want to insert into the linked list.
+     */
     void insertFirst(int data)
     {
         Node *newNode = NULL;
@@ -38,7 +63,13 @@ public:
         this->size++;
     };
 
+/* Deleting the first node in the linked list. */
 public:
+    /**
+     * It deletes the first node in the linked list.
+     * 
+     * @return The first node in the list.
+     */
     Node *deleteFirst()
     {
         if (this->head != NULL)
@@ -54,7 +85,11 @@ public:
         }
     };
 
+/* Printing the list. */
 public:
+    /**
+     * It prints the list.
+     */
     void displayList()
     {
         Node *current = this->head;
--- List.cpp
+++ List.cpp
@@ -1,12 +1,15 @@
 #include "Collector.cpp"
 
+/* It's a circular doubly linked list with a collector */
 class List {
+/* It's declaring the variables that will be used in the class. */
 private:
     Node *head;
     Node *tail;
     int size;
     Collector *collector;
 
+/* It's the constructor of the class. */
 public:
     List()
     {
@@ -16,7 +19,15 @@ public:
         this->collector = new Collector();
     };
 
+/* It's adding a new node to the list. */
 public:
+    /**
+     * If the collector is empty, allocate a new node and add it to the list. 
+     * 
+     * If the collector is not empty, get a node from the collector and add it to the list.
+     * 
+     * @param newData The data that will be stored in the new node.
+     */
     void neW(int newData) {
         if (collector->isEmpty()) { //Collector vacio, MALLOC
             Node *newNode = (Node*)malloc(sizeof(Node));
@@ -67,7 +78,13 @@ public:
         }
     }
 
+/* It's deleting a node from the list and inserting it into the collector list. */
 public:
+    /**
+     * It deletes a node from the list and inserts it into the collector list
+     * 
+     * @param deleteData The data to be deleted from the list.
+     */
     void deletE(int deleteData) {
         Node *current = head;
         Node *temp = head;
@@ -92,7 +109,11 @@ public:
             printf("Se ha eliminado %i de la lista\n\n", deleteData);
         }
     }
+/* It's displaying the list. */
 public:
+    /**
+     * It prints the list
+     */
     void displayList()
     {
         printf(" ------ Displaying LIST con %i de elementos ------\n", size);
